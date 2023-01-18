1b14f16a23ccfe3d1728fea4acf0b1332f43471a
lauragisellesilva <lausilva@emeal.nttdata.com>
2023-01-18 11:02:13 +0100

    Added file creation with studen data closes #7

--- NttData.FileManagement.Business.Logic/Implementations/StudentService.cs
+++ NttData.FileManagement.Business.Logic/Implementations/StudentService.cs
@@ -1,5 +1,7 @@
 ﻿using NttData.FileManagement.Business.Logic.Contracts;
 using NttData.FileManagement.Common.Model;
+using NttData.FileManagement.DataAccess.Repository.Contracts;
+using NttData.FileManagement.DataAccess.Repository.Implementations;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +14,27 @@ namespace NttData.FileManagement.Business.Logic.Implementations
     {
         public bool Add(Student student)
         {
-            throw new NotImplementedException();
+            bool ageIsNotEmpty = false;
+            int age = GetAge(student.Birthday);
+
+            if (age > 0)
+            {
+                student.Age = age;
+                IStudentRepository studentRepository = new StudentRepository();
+                studentRepository.Add(student);
+                ageIsNotEmpty = true;
+            }
+            return ageIsNotEmpty;
+        }
+
+        private int GetAge(DateTime birthday)
+        {
+            DateTime now = DateTime.Today;
+            int edad = DateTime.Today.Year - birthday.Year;
+            if (DateTime.Today < birthday.AddYears(edad))
+                --edad;
+
+            return edad;
         }
     }
 }
